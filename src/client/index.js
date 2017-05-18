fcc9c8f11043fd903cf9b364994e0e27fdb355fe
perillaroc <perillaroc@gmail.com>
2017-05-18 20:38:58 +0800

    add menu in client.

--- src/client/index.js
+++ src/client/index.js
@@ -5,12 +5,30 @@ require('bootstrap-loader');
 require('./main.css');
 
 
-
 window.addEventListener('load', function(event){
-    let container = document.querySelector('#container');
+    let time_bar = document.querySelector('#time_bar');
     function updateTime(){
-        container.innerHTML = moment().format('HH:mm:ss');
+        time_bar.innerHTML = moment().format('HH:mm:ss');
         setTimeout(updateTime, 1000);
     }
     setTimeout(updateTime, 1000);
+
+    let about_button = document.querySelector('#about_button');
+    about_button.addEventListener('click', function(event){
+        jQuery('#about_model').modal();
+    });
+
+    let current_style = 'black';
+    let change_background_button = document.querySelector('#change_background_button');
+    change_background_button.addEventListener('click', function(event){
+        if(current_style === 'black') {
+            time_bar.style.backgroundColor = 'black';
+            time_bar.style.color = 'white';
+            current_style = 'white';
+        } else if(current_style === 'white') {
+            time_bar.style.backgroundColor = 'white';
+            time_bar.style.color = 'black';
+            current_style = 'black';
+        }
+    });
 });
